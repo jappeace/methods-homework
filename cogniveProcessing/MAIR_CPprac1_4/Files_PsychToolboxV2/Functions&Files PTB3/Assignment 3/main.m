f7d5f03d4048bfeb41a41e7adc2cee4cad0cf1ff
Jappie Klooster <jappieklooster@hotmail.com>
2015-09-29 20:56:49 +0200

    finished with the code

--- cogniveProcessing/MAIR_CPprac1_4/Files_PsychToolboxV2/Functions&Files PTB3/Assignment 3/main.m
+++ cogniveProcessing/MAIR_CPprac1_4/Files_PsychToolboxV2/Functions&Files PTB3/Assignment 3/main.m
@@ -1,11 +1,11 @@
 clear all; commandwindow;
-trialcount = 10;
+trialcount = 3;
 stimtime = 1;
 
 results = zeros(trialcount,3);
 [w,screenrect] = openwindow();
 
-writetexttoscreen('focus on the cross.', -30, w, screenrect);
+writetexttoscreen('Focus on the cross.', -30, w, screenrect);
 writetexttoscreen('Press space when the motion stops.', 0, w, screenrect);
 writetexttoscreen('Press space to start.', 30, w,screenrect);
 flips(w,screenrect);
@@ -18,4 +18,25 @@ for i = 1:trialcount
     results(i,:) = concentricmotiontrial(stimtime, w, screenrect);
 end
 Screen closeall;
-plot(results);      
+subplot(2,1,1);
+plot(results);    
+title('raw data');
+xlabel('experiment number');
+ylabel('time');          
+means = [];
+standarts = [];
+
+for i = 1:length(results(1,:))
+    means(i) = mean(results(:,i));
+    standarts(i) = std(results(:,i));
+end          
+subplot(2,1,2);
+bar(means);
+title('Average duration');
+hold on;
+
+errorbar([1 2 3], means, standarts, 'o');
+set(gca, 'xtick', [1 2 3]);
+set(gca, 'xticklabel', {'first', 'second', 'third'});
+
+hold off;
