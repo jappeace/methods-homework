78bbe29ebf01e60f3b0e5182dd88df4a94ea6e8e
Jappie Klooster <jappieklooster@hotmail.com>
2015-09-30 13:23:49 +0200

    moved the plot graph to a seperate file

--- cogniveProcessing/MAIR_CPprac1_4/Files_PsychToolboxV2/Functions&Files PTB3/Assignment 3/main.m
+++ cogniveProcessing/MAIR_CPprac1_4/Files_PsychToolboxV2/Functions&Files PTB3/Assignment 3/main.m
@@ -1,6 +1,6 @@
 clear all; commandwindow;
 trialcount = 10;
-stimtime = 20;
+stimtime = 1;
 
 results = zeros(trialcount,3);
 [w,screenrect] = openwindow();
@@ -17,26 +17,5 @@ for i = 1:trialcount
     disp(strcat('trial: ', num2str(i)));
     results(i,:) = concentricmotiontrial(stimtime, w, screenrect);
 end
+save('results','results');
 Screen closeall;
-subplot(2,1,1);
-plot(results);    
-title('raw data');
-xlabel('experiment number');
-ylabel('time');          
-means = [];
-standarts = [];
-
-for i = 1:length(results(1,:))
-    means(i) = mean(results(:,i));
-    standarts(i) = std(results(:,i));
-end          
-subplot(2,1,2);
-bar(means);
-title('Average duration');
-hold on;
-
-errorbar([1 2 3], means, standarts, 'o');
-set(gca, 'xtick', [1 2 3]);
-set(gca, 'xticklabel', {'first', 'second', 'third'});
-
-hold off;
