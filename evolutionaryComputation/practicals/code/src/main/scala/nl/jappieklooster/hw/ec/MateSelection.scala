60b2cc04b655bd697c64cf027e4ee61275162f0d
Jappie Klooster <jappieklooster@hotmail.com>
2015-11-16 23:29:12 +0100

    tried to improve speed, failed

--- evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/MateSelection.scala
+++ evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/MateSelection.scala
@@ -5,26 +5,35 @@ object MateSelection {
 	def tournamentWinIsParent(
 		valuator : Valuable => Int, bachelors: Population
 	) : PairedPopulation = {
-		def tournement(contenders:Seq[IMember]):Pair = {
-			def sorter(a:IMember, b:IMember) = valuator(a) > valuator(b)
-
-			val father = contenders.drop(2).sortWith(sorter)
-			val mother = contenders.sortWith(sorter)
-			Pair(father.headOption.getOrElse(mother.head), mother.head)
+		def tournement(contender:(IMember, IMember, IMember, IMember)):Pair = {
+			val mother = if (
+				valuator(contender._1) > valuator(contender._2)
+			) contender._1 else contender._2
+			val father = if(
+				valuator(contender._3) > valuator(contender._4)
+			) contender._3 else contender._4
+			Pair(mother, father)
 		}
+		def matching(a:Seq[IMember], b:Seq[IMember]) = a.zip(b.reverse).grouped(2)
 		val mem = bachelors.members
-		val matchup = mem.grouped(4)
-		// first matchup the members into a tournement
-		// to fill 1 pair we need 4 contestents (as 2 will lose)
+		val half = Math.floor(mem.length/2).toInt
+		val revmem = mem.take(half) ++ mem.drop(half).reverse
+
+		val matchup = matching(mem,mem) ++ matching(revmem, revmem)
 		// because we used reverse half can be dropped
 
 		// creating the final structure
-		val brothers = matchup.map(tournement)
+		val brothers = matchup.map(x => {
+
+			// flatten the thing to give myself not a headache later
+			tournement((x.head._1, x.head._1, x.last._1, x.last._2))
+		})
 		val sisters = matchup.map(x => {
-			tournement(x.head +: x.drop(1).reverse)
-		}).toSeq
+			// flatten the thing to give myself not a headache later
+			tournement((x.head._1, x.last._1, x.last._1, x.head._2))
+		})
 
 		// the actuall tournement
-		return PairedPopulation(sisters ++ brothers, bachelors.memberFactory)
+		return PairedPopulation(sisters.toSeq ++ brothers, bachelors.memberFactory)
 	}
 }
