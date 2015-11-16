afdcf645d56460791858e2bac36a9967213ec7b7
Jappie Klooster <jappieklooster@hotmail.com>
2015-11-16 23:01:08 +0100

    made some mistakes, resetting them now

--- evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/Main.scala
+++ evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/Main.scala
@@ -5,17 +5,19 @@ import org.slf4j.LoggerFactory
 import scala.util.Random
 
 object Main{
+	case class Configurations(strLength:Int,popSize:Int,genCount:Int, evolution:Evolution)
 	val log = LoggerFactory.getLogger(Main.getClass)
 	val random = Random
 	def main(args:Array[String]){
 		log.info("starting ea")
 
-		val length = 20
+		val stringLength = 20
+		val populationSize = 20
 		val population = Population.createOneZeros(
 			MemberFactories.globalRandomizedGenes(
-				random.shuffle(0.to(length))),
-				length,
-				40
+				random.shuffle(0.to(stringLength))),
+				stringLength,
+				populationSize
 		)
 		val evolution = new Evolution(
 			Valuation.uniformlyScaledCountOnes,
--- evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/MateSelection.scala
+++ evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/MateSelection.scala
@@ -5,38 +5,24 @@ object MateSelection {
 	def tournamentWinIsParent(
 		valuator : Valuable => Int, bachelors: Population
 	) : PairedPopulation = {
-		def tournement(contender:(IMember, IMember, IMember, IMember)):Pair = {
-			val mother = if (
-				valuator(contender._1) > valuator(contender._2)
-			) contender._1 else contender._2
-			val father = if(
-				valuator(contender._3) > valuator(contender._4)
-			) contender._3 else contender._4
-			Pair(mother, father)
+		def tournement(contenders:Seq[IMember]):Pair = {
+			def sorter(a:IMember, b:IMember) = valuator(a) > valuator(b)
+
+			val father = contenders.drop(2).sortWith(sorter)
+			val mother = contenders.sortWith(sorter)
+			Pair(father.headOption.getOrElse(mother.head), mother.head)
 		}
 		val mem = bachelors.members
-		val matchup = mem
+		val matchup = mem.grouped(4)
 		// first matchup the members into a tournement
 		// to fill 1 pair we need 4 contestents (as 2 will lose)
-		.zip(
-			mem.slice(1, mem.length) :+ mem.head
-		).zip(
-			mem.reverse
-		).zip(
-			(mem.slice(1, mem.length) :+ mem.head).reverse
-		)
 		// because we used reverse half can be dropped
-		.drop(Math.floor(mem.length/2).toInt)
 
 		// creating the final structure
-		val brothers = matchup.map(x => {
-			// flatten the thing to give myself not a headache later
-			tournement((x._1._1._1, x._1._1._2, x._1._2, x._2))
-		})
+		val brothers = matchup.map(tournement)
 		val sisters = matchup.map(x => {
-			// flatten the thing to give myself not a headache later
-			tournement((x._1._1._1, x._2, x._1._1._2, x._2))
-		})
+			tournement(x.head +: x.drop(1).reverse)
+		}).toSeq
 
 		// the actuall tournement
 		return PairedPopulation(sisters ++ brothers, bachelors.memberFactory)
