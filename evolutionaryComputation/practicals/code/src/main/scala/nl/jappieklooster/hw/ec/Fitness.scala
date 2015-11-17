5c447e3d2c22a4a5802d45253a5f640f467554c1
Jappie Klooster <jappieklooster@hotmail.com>
2015-11-17 16:11:26 +0100

    fixed the issues indicated by the unit tests

--- evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/Fitness.scala
+++ evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/Fitness.scala
@@ -13,16 +13,14 @@ object Fitness {
 			// select the elements where 1
 			filter(_._1 == '1').
 			// throw away the elements and keep the indeci
-			map(_._2).
+			map(_._2+1).
 			// result
 			sum
 	def blockValuation(block:Seq[Float])(s:IHasFitness) = s.getFitness.
-			// make a map like thing of it where the index is the key
-			zipWithIndex.
-			// select the elements where 1
-			filter(_._1 == '1').
-			// throw away the elements and keep the indeci
-			map(x => block.apply(x._2 % block.size)).
-			// result
-			sum
+			// make blocks of the string
+			grouped(block.size-1).
+			map(
+				// get the value specified in the block
+				str => block.apply(str.count(c=>c=='1'))
+			).sum
 }
