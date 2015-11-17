16437241dc96cfb8a8cdc0a65f8369d6e4c4005f
Jappie Klooster <jappieklooster@hotmail.com>
2015-11-17 15:26:30 +0100

    reduced the number of methods by moving the static code into the companion object.

--- evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/PairedPopulation.scala
+++ evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/PairedPopulation.scala
@@ -29,52 +29,39 @@ case class PairedPopulation(members:Seq[Pair], memberFactory:String => IMember){
 			memberFactory
 		)
 	}
-	/**
-	 * @param parents
-	 * @return
-	 */
-	def uniform : Population = {
+}
+object OffspringGenerator{
+	def uniformCross(x:PairedPopulation) : Population = x.pairedToPop(
 		/* kindoff a weird implementation, but it works because
 		 1==1V0==0, then the selection is irrelevant
 		 but iff 1==0 or 0==1, then the randomGene will just
 		 create a 1 or 0 without looking at the parents.
 		*/
-		pairedToPop(
-			genes =>
-				// where the genes are equal will keep the same, else random
-				genes.map(fm => if (fm._1 == fm._2) fm._1 else Main.randomGene)
-		)
-	}
-	def singlePoint : Population = {
-		pairedToPop(
-			genes => {
-				val point = Main.random.nextInt(genes.length)
-				// where the genes are equal will keep the same, else random
-				genes.zipWithIndex.map(fm => {
-					if (point < fm._2) fm._1._1 else fm._1._2
-				})
-			}
-		)
-	}
-	def twoPoint : Population = {
-		pairedToPop(
-			genes => {
-				val end = Main.random.nextInt(genes.length)
-				val start = Main.random.nextInt(genes.length - end)
-				// where the genes are equal will keep the same, else random
-				genes.zipWithIndex.map(fm => {
-					if(fm._2 < start) fm._1._1 else{
-						if(fm._2 < end){
-							fm._1._2
-						}else fm._1._1
-					}
-				})
-			}
-		)
-	}
-}
-object OffspringGenerator{
-	def uniformCross(x:PairedPopulation) : Population = x.uniform
-	def singlePointCross(x:PairedPopulation) : Population = x.singlePoint
-	def twoPointCross(x:PairedPopulation) : Population = x.twoPoint
+		genes =>
+			// where the genes are equal will keep the same, else random
+			genes.map(fm => if (fm._1 == fm._2) fm._1 else Main.randomGene)
+	)
+	def singlePointCross(x:PairedPopulation) : Population = x.pairedToPop(
+		genes => {
+			val point = Main.random.nextInt(genes.length)
+			// where the genes are equal will keep the same, else random
+			genes.zipWithIndex.map(fm => {
+				if (point < fm._2) fm._1._1 else fm._1._2
+			})
+		}
+	)
+	def twoPointCross(x:PairedPopulation) : Population = x.pairedToPop(
+		genes => {
+			val end = Main.random.nextInt(genes.length)
+			val start = Main.random.nextInt(genes.length - end)
+			// where the genes are equal will keep the same, else random
+			genes.zipWithIndex.map(fm => {
+				if(fm._2 < start) fm._1._1 else{
+					if(fm._2 < end){
+						fm._1._2
+					}else fm._1._1
+				}
+			})
+		}
+	)
 }
