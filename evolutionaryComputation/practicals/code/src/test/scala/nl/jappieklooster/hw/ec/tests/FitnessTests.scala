32fa53599f6742522bf953d61749c543a748d6ba
Jappie Klooster <jappieklooster@hotmail.com>
2015-11-17 15:10:01 +0100

    so I finished writing the tests, I did it corectly because I can calulate it and 3 of the 4 valuations are wrong. I'm thinking about first writing all the tests and see how much stuff is wrong before fixing it.

--- evolutionaryComputation/practicals/code/src/test/scala/nl/jappieklooster/hw/ec/tests/FitnessTests.scala
+++ evolutionaryComputation/practicals/code/src/test/scala/nl/jappieklooster/hw/ec/tests/FitnessTests.scala
@@ -10,22 +10,28 @@ class FitnessTests extends FlatSpec with Matchers{
 	 * create a known subject which we can count manually
 	 */
 	val subject = new IHasFitness {
-		val str = "11111" + "01111" + "10110" + "00001" + "00000" + "10000"
-		//			12345	  67890		12345	  67890		12345	  6
-		//so linearly =1+2+3+4+5+7+8+9+10+11+13+14+20+26 = 133
+		val str = "1111" + "0111" + "1010" + "1000" + "0000"
+		//		   1234     5678     9012     3
+		// so linearly scaled = 1+2+3+4 + 6+7+8 + 9+ 11+ 13 = 64
+		// decept: 4	+	0	+ 	1	+	2	+ 3
+		// non: 	4	+	0	+ 	0.5	+	1	+ 1.5
 
 		override def getFitness: String = str
 	}
 
-	"Linearly counting fitness " should "count subject as 14" in {
-		Fitness.uniformlyScaledCountOnes(subject) should be (14)
+	"Linearly counting fitness " should "count subject as 10" in {
+		Fitness.uniformlyScaledCountOnes(subject) should be (10)
 	}
-	"Linearly scaled fitness " should "count subject as 133" in {
-		Fitness.linearlyScaledCountOnes(subject) should be (133)
+	"Linearly scaled fitness " should "count subject as 64" in {
+		Fitness.linearlyScaledCountOnes(subject) should be (64)
 	}
 
 	val deciptive = List(4f,0f,1f,2f,3f)
-	"deceptive trap fitness" should "count subject as" in {
-		Fitness.blockValuation(deciptive)(subject) should be (1)
+	"deceptive trap fitness" should "count subject as 10" in {
+		Fitness.blockValuation(deciptive)(subject) should be (10)
+	}
+	val nondecept = List(4f, 0f, 0.5f, 1f, 1.5f)
+	"nondeceptive trap fitness" should "count subject as 7" in {
+		Fitness.blockValuation(nondecept)(subject) should be (7)
 	}
 }
