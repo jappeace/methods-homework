73030db165a837b0a78e8d061ae9d4f24faa9f2e
Jappie Klooster <jappieklooster@hotmail.com>
2015-11-23 12:04:42 +0100

    readded truncate elitism

--- evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/FittestFilter.scala
+++ evolutionaryComputation/practicals/code/src/main/scala/nl/jappieklooster/hw/ec/FittestFilter.scala
@@ -9,6 +9,17 @@ object FittestFilter {
 		children
 	}
 
+	def truncateElitism(
+			valuation : IHasFitness => Int,
+			parents : Population,
+			children : Population
+			) : Population = {
+		val combined = parents ++ children
+		// end up sorting anyways, no idea why you would do tournement selection
+		val sorted = combined.sortWith((x, y) => x.fitness > y.fitness)
+		parents.copy(members = sorted.take(parents.length))
+	}
+
 	def tournementElitism(
 		parents : Population,
 		children : Population
