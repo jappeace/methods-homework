0c35ace8fe0033edd22b29f852c2d9a6bc59f848
Jappie Klooster <jappieklooster@hotmail.com>
2016-01-31 14:12:58 +0100

    add license to all the new files (ie, yes you can use it, no don't bother me with it)

--- evolutionaryComputation/2-practical/code/src/main/scala/nl/jappieklooster/hw/ec/model/Vertex.scala
+++ evolutionaryComputation/2-practical/code/src/main/scala/nl/jappieklooster/hw/ec/model/Vertex.scala
@@ -1,3 +1,18 @@
+// Evolutionary Computing Analyzer, it analyzes various genetic algoritms
+// Copyright (C) 2015 Jappie Klooster
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/\>.
 package nl.jappieklooster.hw.ec.model
 
 case class Vertex(id:Int, connections:Seq[Int]) {
