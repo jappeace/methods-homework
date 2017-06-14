d5a5609e587faea38f6331ea6b8b2ea23c81e35a
Jappie Klooster <jappieklooster@hotmail.com>
2017-06-14 15:02:26 +0200

    Little script for doing the presentation

---
+++ thesis/dopresentation.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# we can't write directly into the same file we're reading from.
+# therefore we work around it like this:
+# do the operation
+cat presentation.html | sed "s/&#57344;&#57345;&#57345;//g" > presentation.html.result
+# take the pipe result and overwrite original
+cp presentation.html.result presentation.html
+# remove pipe result
+rm presentation.html.result
+
+# this opens a new tab in firefox
+firefox presentation.html
