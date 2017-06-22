8ad2447a5c5f465c3aa59218e375029cf38ab24b
Jappie Klooster <jappieklooster@hotmail.com>
2017-06-22 18:13:43 +0200

    Undo multipluxing upload

--- thesis/publish-presentation.sh
+++ thesis/publish-presentation.sh
@@ -5,11 +5,12 @@ socketio=socket.io-1.4.5.js
 
 mkdir -p output
 
-cat presentation_client.html | sed "s/&#57344;&#57345;&#57345;//g" > output/index.html
-cat presentation.html | sed "s/&#57344;&#57345;&#57345;//g" > output/master.html
+
+cat presentation.html | sed "s/&#57344;&#57345;&#57345;//g" > output/index.html
 
 rsync -avc $revealjs output/
 rsync -avc img output/
 
+cp $socketio output/
 
 rsync -avc --delete output/ root@jappieklooster.nl:/var/www/html/presents/thesis
