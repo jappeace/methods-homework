b2988d02f632dedc942fd6b2183f4d861c2e7a25
Jappie Klooster <jappieklooster@hotmail.com>
2017-06-22 18:14:28 +0200

    Also delete socketio stuff

--- thesis/publish-presentation.sh
+++ thesis/publish-presentation.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 revealjs=reveal.js-3.5.0
-socketio=socket.io-1.4.5.js
 
 mkdir -p output
 
@@ -10,7 +9,5 @@ cat presentation.html | sed "s/&#57344;&#57345;&#57345;//g" > output/index.html
 
 rsync -avc $revealjs output/
 rsync -avc img output/
-
-cp $socketio output/
 
 rsync -avc --delete output/ root@jappieklooster.nl:/var/www/html/presents/thesis
